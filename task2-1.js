bb46a123ad44b77ae9861efcf7cf9f40571c1bb2
kotlasrinivasu <121429003+kotlasrinivasu@users.noreply.github.com>
2023-07-27 12:57:52 +0530

    Update task2-1.js

--- task2-1.js
+++ task2-1.js
@@ -1,11 +1,34 @@
-var mysql=require('mysql');
-var con=mysql.createConnection({
-    host:"localhost",
-    user:"root",
-    pass:""
+const mysql = require('mysql');
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'mysql123',
+  database: 'employees'
 });
-   con.connect(function(err){
-    if(err) throw err;
-    console.log("Connected!");
+function createDatabase(employees) {
+  connection.query(`CREATE DATABASE IF NOT EXISTS employees`, (error, results) => {
+    if (error) throw error;
+    console.log(`Database "employees" created successfully.`);
+  });
+}
 
-})
+function createTable() {
+  const sql = `
+    CREATE TABLE IF NOT EXISTS employees (
+      id INT AUTO_INCREMENT PRIMARY KEY,
+      name VARCHAR(255),
+      department VARCHAR(100),
+      salary DECIMAL(10, 2)
+    )
+  `;
+  connection.query(sql, (error, results) => {
+    if (error) throw error;
+    console.log('Table "employees" created successfully.');
+  });
+}
+
+module.exports = {
+  connection,
+  createDatabase,
+  createTable,
+};
